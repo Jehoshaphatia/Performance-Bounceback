8947e95901e833c5eb32abe46610361c29542413
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-12-18 01:11:38 +0100

    Created  Ball Destroy, Modified Quality Settings

---
+++ Assets/PerformanceBounceback/Scripts/BallDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () {
+		Invoke ("Destroy", 5f);
+		
+	}
+	
+	// Update is called once per frame
+	void Destroy () {
+		gameObject.SetActive (false);
+	}
+
+	void OnDisable(){
+		CancelInvoke ();
+	}
+}
